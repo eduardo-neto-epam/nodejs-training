65c4b3c35bb18ccc909d15f313ac12ffc29fa29f
Eduardo Neto <Eduardo_Neto@epam.com>
2021-09-26 19:22:03 +0100

    Task 1.2 completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello');
+const fs = require('fs');
+const csv = require('csvtojson');
+
+const main = async () => {
+    const txtDir = './txt';
+    const txtFilePath = './txt/txt-result.txt';
+    try {
+        if (!fs.existsSync(txtDir)) {
+            fs.mkdirSync(txtDir);
+        }
+        csv()
+        .fromFile('./csv/csv-sample.csv')
+        .subscribe(async(json, lineNumber) => {
+            const data = JSON.stringify(json);
+            if (lineNumber === 0) {
+                await fs.promises.writeFile(txtFilePath, data);
+            } else {
+                await fs.promises.appendFile(txtFilePath, `\n${data}`);
+            }
+        });
+    } catch(error) {
+        console.error(error);
+    };
+};
+
+main();

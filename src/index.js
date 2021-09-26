c5526eb5c15d024397334196883cc4f1516c26bc
Eduardo Neto <Eduardo_Neto@epam.com>
2021-09-26 13:31:20 +0100

    task1.1 completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Type a string to reverse: '
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {   
+    const reversedStr = line.trim().split("").reverse().join("");
+    console.log(reversedStr);
+    rl.prompt();
+}).on('close', ()=> {
+    console.log('Program terminated!');
+    process.exit(0);
+})

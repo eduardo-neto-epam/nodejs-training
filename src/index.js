8b4f8ebb7f5563a420e6e5e7bddf39eef75ee429
Eduardo Neto <Eduardo_Neto@epam.com>
2021-09-27 13:02:28 +0100

    improved error handling
    added script completion log

--- src/index.js
+++ src/index.js
@@ -5,23 +5,37 @@ const csv = require('csvtojson');
 const main = async () => {
     const txtDir = path.join(__dirname, '../', 'txt');
     const txtFilePath = path.join(__dirname, '../', 'txt/txt-result.txt');
-    try {
         if (!fs.existsSync(txtDir)) {
-            fs.mkdirSync(txtDir);
+            try {
+                await fs.promises.mkdir(txtDir);
+            } catch(err) {
+                return console.log('Error: ', err.message);
+            }
         }
         csv()
         .fromFile(path.join(__dirname, '../', 'csv/csv-sample.csv'))
         .subscribe(async(json, lineNumber) => {
-            const data = JSON.stringify(json);
-            if (lineNumber === 0) {
-                await fs.promises.writeFile(txtFilePath, data);
-            } else {
-                await fs.promises.appendFile(txtFilePath, `\n${data}`);
+            try {
+                const data = JSON.stringify(json);
+                if (lineNumber === 0) {
+                    try {
+                        await fs.promises.writeFile(txtFilePath, data)
+                    } catch(err) {
+                        throw new Error(`on writeFile: ${err.message}`)
+                    }
+                } else {
+                    try {
+                        await fs.promises.appendFile(txtFilePath, `\n${data}`);
+                    } catch(err) {
+                        throw new Error(`on appendFile: ${err.message}`)
+                    }
+                }
+            } catch(err) {
+                console.error(`Error ${err.message}`);
             }
-        });
-    } catch(error) {
-        console.error(error);
-    };
+        }, (err) => {
+            return console.error(`Error ${err.message}`);
+        }, () => console.log('File conversion script completed'))
 };
 
 main();

a8e31d36a628474938f05b20a03a65df5218b8d3
Eduardo Neto <Eduardo_Neto@epam.com>
2021-09-27 10:54:07 +0100

    updated paths to use path module

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
+const path = require('path');
 const csv = require('csvtojson');
 
 const main = async () => {
-    const txtDir = './txt';
-    const txtFilePath = './txt/txt-result.txt';
+    const txtDir = path.join(__dirname, '../', 'txt');
+    const txtFilePath = path.join(__dirname, '../', 'txt/txt-result.txt');
     try {
         if (!fs.existsSync(txtDir)) {
             fs.mkdirSync(txtDir);
         }
         csv()
-        .fromFile('./csv/csv-sample.csv')
+        .fromFile(path.join(__dirname, '../', 'csv/csv-sample.csv'))
         .subscribe(async(json, lineNumber) => {
             const data = JSON.stringify(json);
             if (lineNumber === 0) {

3d4feffb7f357527073dd339a80de700d6b5aa83
Eduardo Neto <Eduardo_Neto@epam.com>
2021-10-03 23:32:45 +0100

    Extracted code into smaller functions Extracted constants into seperate file Extracted functions into seperate folder Updated code to use a writeStream.

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    TXT_DIR: 'txt',
+    TXT_FILE_NAME: 'txt-result.txt',
+    CSV_FILE_PATH: 'csv/csv-sample.csv',
+};
--- src/index.js
+++ src/index.js
@@ -1,41 +1,23 @@
-const fs = require('fs');
 const path = require('path');
 const csv = require('csvtojson');
+const { TXT_DIR, TXT_FILE_NAME, CSV_FILE_PATH } = require('./constants');
+const { 
+    makeDirIfNotExistsAndReturnFilePath,
+    subscribeHandler,
+    subscribeErrorHandler,
+    endOfConversionNotice,
+ } = require('./utils');
 
-const main = async () => {
-    const txtDir = path.join(__dirname, '../', 'txt');
-    const txtFilePath = path.join(__dirname, '../', 'txt/txt-result.txt');
-        if (!fs.existsSync(txtDir)) {
-            try {
-                await fs.promises.mkdir(txtDir);
-            } catch(err) {
-                return console.log('Error: ', err.message);
-            }
-        }
-        csv()
-        .fromFile(path.join(__dirname, '../', 'csv/csv-sample.csv'))
-        .subscribe(async(json, lineNumber) => {
-            try {
-                const data = JSON.stringify(json);
-                if (lineNumber === 0) {
-                    try {
-                        await fs.promises.writeFile(txtFilePath, data)
-                    } catch(err) {
-                        throw new Error(`on writeFile: ${err.message}`)
-                    }
-                } else {
-                    try {
-                        await fs.promises.appendFile(txtFilePath, `\n${data}`);
-                    } catch(err) {
-                        throw new Error(`on appendFile: ${err.message}`)
-                    }
-                }
-            } catch(err) {
-                console.error(`Error ${err.message}`);
-            }
-        }, (err) => {
-            return console.error(`Error ${err.message}`);
-        }, () => console.log('File conversion script completed'))
+const csvFilePath = path.join(__dirname, '../', CSV_FILE_PATH);
+
+const csvToTxtConverter = (csvFilePath) => {
+    makeDirIfNotExistsAndReturnFilePath(TXT_DIR, TXT_FILE_NAME)
+        .then((txtFilePath) => {
+            if (!txtFilePath) return console.error('Error on creating path to save txt file');
+            csv()
+            .fromFile(csvFilePath)
+            .subscribe(subscribeHandler(txtFilePath), subscribeErrorHandler, endOfConversionNotice)
+        })
 };
 
-main();
+csvToTxtConverter(csvFilePath);
